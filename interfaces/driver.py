e90f5dedcf606905873ac8c1c6ec0b71cbcbfb29
PascalCorpsman <corpsman@corpsman.de>
2025-11-22 09:58:33 +0100

    ADD: convert python2 to python3 using ChatGPT

--- interfaces/driver.py
+++ interfaces/driver.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import os, sys
 
 state = [
@@ -9,70 +9,89 @@ state = [
     [ 0, 0, 0, 0, 0, 0, 0 ],
     [ 0, 0, 0, 1, 0, 0, 0 ]]
 
+
 def drop(column, number):
     if state[0][column] != 0:
-        print "It is full!"
+        print("It is full!")
         raise Exception("")
-    elif 0<=column<=6:
-        for row in xrange(5,-1,-1):
+    elif 0 <= column <= 6:
+        for row in range(5, -1, -1):
             if row == 5:
                 if state[5][column] == 0:
                     state[5][column] = number
                     break
-            elif state[row+1][column] != 0 and state[row][column] == 0:
+            elif state[row + 1][column] != 0 and state[row][column] == 0:
                 state[row][column] = number
                 break
         else:
-            print "Invalid move"
+            print("Invalid move")
             raise Exception("")
 
+
 def PrintState():
-    print "\n".join(str(x).replace('0',' ').replace(',',' ').replace('1','X').replace('2','O') for x in state)
-    print "--------------------"
-    print " 0  1  2  3  4  5  6\n"
+    print("\n".join(
+        str(x)
+        .replace('0', ' ')
+        .replace(',', ' ')
+        .replace('1', 'X')
+        .replace('2', 'O')
+        for x in state
+    ))
+    print("--------------------")
+    print(" 0  1  2  3  4  5  6\n")
+
 
 while True:
     column = -1
     resp = ""
+
     while True:
         while True:
             allOK = False
             PrintState()
             try:
-                ans = raw_input('"q" to quit, or column number (0-6): --> ')
+                ans = input('"q" to quit, or column number (0-6): --> ')
                 if ans == "q":
                     sys.exit(0)
                 column = int(ans)
                 drop(column, 2)
                 allOK = True
             except SystemExit:
-                raise;
-            except:
+                raise
+            except Exception:
                 pass
+
             if allOK:
                 break
 
         PrintState()
         s = ""
-        for y in xrange(0,6):
-            for x in xrange(0,7):
+        for y in range(0, 6):
+            for x in range(0, 7):
                 if state[y][x] != 0:
-                    s+= " " + {1:"o",2:"y"}[state[y][x]] + str(y) + str(x)
-        #print "./C++/score4 %s -level 7" % s
-        pipe = os.popen("./interfaces/engine.exe %s -level 7" % s)
+                    s += " " + {1: "o", 2: "y"}[state[y][x]] + str(y) + str(x)
+
+        cmd = "./interfaces/engine.exe %s -level 7" % s
+        pipe = os.popen(cmd)
+
         try:
             resp = pipe.readlines()[0]
             resp = int(resp)
-        except:
-            if resp.split()[1] == "win" or resp.split()[0] == "No":
-                print resp
-                print "GAME OVER"
+        except Exception:
+            # Engine signals game over via text
+            parts = resp.split()
+            if len(parts) > 1 and (parts[1] == "win" or parts[0] == "No"):
+                print(resp)
+                print("GAME OVER")
                 sys.exit(0)
+
         exitCode = pipe.close()
-        if None != exitCode:
+
+        if exitCode is not None:
             drop(resp, 1)
             PrintState()
-            print "Game finished."
+            print("Game finished.")
             sys.exit(0)
-        print "Computer chose", resp, "\n"
+
+        print("Computer chose", resp, "\n")
         drop(resp, 1)
